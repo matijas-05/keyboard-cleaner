e5938fdc48fe2e61dc2677818ef8826027d4e7c7
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-10-07 16:34:35 +0200

    feat: setup cmake, add logging and tray libs, add simple tray menu

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#define TRAY_APPINDICATOR 1
+#include "log.c/src/log.h"
+#include "tray/tray.h"
 
-int main(int argc, char* argv[]) {
-    printf("%d\n%s", argc, *argv);
+int main(void) {
+    struct tray_menu menu[] = {
+        {
+            .text = "Keyboard Cleaner",
+            .disabled = 1,
+        },
+        {
+            .text = "Disable keyboard",
+        },
+    };
+    struct tray tray = {.menu = menu, .icon = ""};
+
+    log_info("Starting tray...");
+    tray_init(&tray);
+    while (tray_loop(1) == 0) {
+    }
+    tray_exit();
+
     return 0;
 }
