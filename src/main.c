fe2c2e2707e8b033f930b32be6c75c21bc2966dc
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-10-08 02:34:05 +0200

    feat: disable keyboard

--- src/main.c
+++ src/main.c
@@ -1,21 +1,44 @@
+#include <stdlib.h>
+
 #define TRAY_APPINDICATOR 1
 #include "log.c/src/log.h"
 #include "tray/tray.h"
 
+bool disabled = false;
+
+void toggle_keyboard(struct tray_menu* element) {
+    // TODO: Named pipe
+    if (!disabled) {
+        system("pkexec ./keyboard-cleaner-cli disable");
+        element->checked = true;
+    } else {
+        system("pkexec ./keyboard-cleaner-cli enable");
+        element->checked = false;
+    }
+}
+
+void quit(void) {
+    exit(0);
+}
+
 int main(void) {
-    struct tray_menu menu[] = {
-        {
-            .text = "Keyboard Cleaner",
-            .disabled = 1,
-        },
-        {
-            .text = "Disable keyboard",
-        },
-    };
+    struct tray_menu menu[] = {{
+                                   .text = "Disable keyboard",
+                                   .cb = &toggle_keyboard,
+                               },
+                               {
+                                   .text = "Quit",
+                                   .cb = (void (*)(struct tray_menu*))(&quit),
+                               },
+                               {.text = NULL}};
     struct tray tray = {.menu = menu, .icon = ""};
 
     log_info("Starting tray...");
-    tray_init(&tray);
+    if (tray_init(&tray) == -1) {
+        log_error("Failed to create tray icon");
+        return 1;
+    }
+
     while (tray_loop(1) == 0) {
     }
     tray_exit();
