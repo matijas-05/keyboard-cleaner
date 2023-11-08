57cc22595c3aee40a3479596b12fac739baf0fe9
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-11-08 03:38:36 +0100

    chore: split build into debug and release

--- setup.bat
+++ setup.bat
@@ -1,1 +1,2 @@
-cmake -S . -B build -G "MinGW Makefiles"
+cmake -S . -B build\debug -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build\release -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cmake -S . -B build/ -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang
+cmake -S . -B build/debug/ -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build/release/ -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang -DCMAKE_BUILD_TYPE=Release
